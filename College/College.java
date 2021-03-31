8dd04fd3db172611e3f71acbcf7785bf48e2f216
Ramkumar005 <63731813+Ramkumar005@users.noreply.github.com>
2021-03-31 14:10:49 +0530

    Add files via upload

---
+++ College/College.java
@@ -0,0 +1,18 @@
+package org.College;
+
+public class College {
+
+	public void collegeName() {
+		System.out.println("VelTech University");
+	}
+
+	public void collegeCode() {
+		System.out.println("00500");
+	}
+
+	public void collegeRank() {
+		System.out.println("Top 10 University in Chennai");
+	}
+
+	
+}
